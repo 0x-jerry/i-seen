050aa82f7d729d7b018bc741a64215a6951dab12
Jerry Wang <x.jerry.wang@gmail.com>
2022-12-04 19:42:45 +0800

    chore: update db

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,8 +5,9 @@ import { db } from '@/data'
 <template>
   <div class="text-4xl mt-30px ml-50px">I Seen</div>
   <hr class="border-0 border-(t gray-3) mb-4" />
-  <div class="px-50px flex flex-wrap gap-8">
+  <div class="px-50px flex flex-wrap gap-8 justify-between">
     <Card v-for="item in db" v-bind="item" />
+    <div class="w-240px h-1px" v-for="_ in 8"></div>
   </div>
 </template>
 
